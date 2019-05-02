1155da9de6e9a8711339574b305c75bbb6585cf7
Dmitry <viking19-98@mail.ru>
2019-05-02 15:04:11 +0700

    b

---
+++ main.js
@@ -0,0 +1,10 @@
+function func(){
+    var el=document.getElementById("hob") 
+    el.classList.remove("hobbi")
+}
+function func2(){
+    var ee=document.getElementById("imag")
+    var el=document.getElementById("hob") 
+    el.classList.add("hobbi")
+    ee.classList.remove("image")
+}
